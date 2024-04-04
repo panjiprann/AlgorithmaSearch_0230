d621c77cfc7f8d92cffa47f57a5a2728d51531ac
panjiprann <panjipranantya20@gmail.com>
2024-04-04 16:37:10 +0700

    membuat input 1

--- algo.cpp
+++ algo.cpp
@@ -6,6 +6,15 @@ int n;
 int i;
 
 void input (){
+    while (true) 
+    {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        if ((n > 0)&&(n <= 20))
+           break;
+        else
+           cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+    }
 
 }
 
