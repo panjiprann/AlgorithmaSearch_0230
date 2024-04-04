672e4ee118e58b4140adc496fc7bdc764956dc9d
panjiprann <panjipranantya20@gmail.com>
2024-04-04 16:43:47 +0700

    memperbaiki input 2

--- algo.cpp
+++ algo.cpp
@@ -14,7 +14,7 @@ void input (){
            break;
         else
            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
-    }
+    
 
 }
 cout << "\n---------------\n" ;
@@ -25,6 +25,8 @@ for (i = 0; i < n; ++i)
     cout << "<" << (i + 1) << ">: ";
     cin >> arr[i] ;
  }
+ 
+}
 
 void LinearSearch()
 {
