ad89ad0583e0dce96a74bafbfd62db98025a8c0d
panjiprann <panjipranantya20@gmail.com>
2024-04-04 16:42:26 +0700

    input 2

--- algo.cpp
+++ algo.cpp
@@ -17,6 +17,14 @@ void input (){
     }
 
 }
+cout << "\n---------------\n" ;
+cout << "enter array elements\n" ;
+cout << "-----------------\n" ;
+for (i = 0; i < n; ++i)
+ {
+    cout << "<" << (i + 1) << ">: ";
+    cin >> arr[i] ;
+ }
 
 void LinearSearch()
 {
